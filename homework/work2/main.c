3fafa183831fbc802dfb2a7186d3d6a87045f14d
Vegetab1e <lizhi88038@gmail.com>
2020-10-14 08:25:50 +0800

    2

--- homework/work2/main.c
+++ homework/work2/main.c
@@ -521,11 +521,11 @@ int genetic_main(Ptr_config config)
 int main (int argc, char* argv[]) {
 	// Create or read a new configuration.
 	Ptr_config config = (Ptr_config) malloc (sizeof(struct Chromosome_configuration));
-	config->total_chrom = 400;
+	config->total_chrom = 40;
 	config->max_iter_best = 100;
 	config->max_iter = 100;
 	config->chrom_length = 30;
-	config->mutate_percentage = 25;
+	config->mutate_percentage = 12;
 
 
 	// Call genetic main method.
